cdeedf0740cc3eebc73ccae369f5e08177ab4193
Marcelo Noskoski <marce.noskoski@gmail.com>
2022-08-30 16:43:58 -0300

    add contents

--- terraform-aws/file-aws-provider.tf
+++ terraform-aws/file-aws-provider.tf
@@ -6,6 +6,3 @@ resource "aws_instance" "aula-devops02" {
   ami           = "ami-090fa75af13c156b4"
   instance_type = "t2.micro"
 }
-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
-
-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
