495ac8bd831700fd91a543403c20ed7839a1d208
Marcelo Noskoski <marce.noskoski@gmail.com>
2022-08-30 16:23:29 -0300

    add contents

--- terraform-aws/file-aws-provider.tf
+++ terraform-aws/file-aws-provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
-resource "aws_instance" "aula-devops01" {
+resource "aws_instance" "aula-devops02" {
   ami           = "ami-090fa75af13c156b4"
   instance_type = "t2.micro"
 }
